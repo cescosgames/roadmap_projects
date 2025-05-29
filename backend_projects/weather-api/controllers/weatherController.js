c8799d3430b37945f78c2f556b1b1590ddfe5171
Cesco <146201884+cescosgames@users.noreply.github.com>
2025-05-29 11:25:30 -0400

    Update weatherController.js

--- backend_projects/weather-api/controllers/weatherController.js
+++ backend_projects/weather-api/controllers/weatherController.js
@@ -1,6 +1,6 @@
 // controller for our weather fetch
 import axios from "axios";
-// import { getWeatherRedis } from "../caching/redisCache.js";
+import { getWeatherRedis } from "../caching/redisCache.js";
 
 // our api key to access the info
 const apiKey = process.env.VC_WEATHER_KEY; // our visual crossing key
@@ -8,23 +8,23 @@ const apiKey = process.env.VC_WEATHER_KEY; // our visual crossing key
 export const getWeather = async (req, res) => {
     const { city } = req.params; // destructure our city
 
-    // // trying to use our redis caching
-    // try {
-    //     const response = await getWeatherRedis(city);
-    //     res.json(response.data)
-    // } catch (error) {
-    //     console.error('error fetching weather data, check spelling or try again later:', error.message);
-    //     res.status(500).json({ error: 'failed to fetch weather data, check spelling or try again later' });
-    // }
-
-    // without redis, just fetching from API no cache option
+    // trying to use our redis caching
     try {
-        // get our response using axios.get, this is like fetch
-        const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`);
-        res.json(response.data) // express method to send our json to the frontend
+        const response = await getWeatherRedis(city);
+        res.json(response)
     } catch (error) {
-        // error handling
         console.error('error fetching weather data, check spelling or try again later:', error.message);
         res.status(500).json({ error: 'failed to fetch weather data, check spelling or try again later' });
     }
+
+    // // without redis, just fetching from API no cache option
+    // try {
+    //     // get our response using axios.get, this is like fetch
+    //     const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`);
+    //     res.json(response.data) // express method to send our json to the frontend
+    // } catch (error) {
+    //     // error handling
+    //     console.error('error fetching weather data, check spelling or try again later:', error.message);
+    //     res.status(500).json({ error: 'failed to fetch weather data, check spelling or try again later' });
+    // }
 };
